7c41b810ccbe55f16f7af1aa2d1700d4627fd370
Eric Park <ericparkmusic@gmail.com>
2021-09-28 17:25:18 -0700

    Add main sections

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import Hero from './containers/Hero';
 import Footer from './containers/Footer';
 import About from './containers/About';
 import Facts from './containers/Facts';
+import Skills from './containers/Skills';
+import Resume from './containers/Resume';
+import Portfolio from './containers/Portfolio';
+import Contact from './containers/Contact';
 
 const App = () => (
   <>
+    <button type="button" class="mobile-nav-toggle d-xl-none">
+      <i class="icofont-navigation-menu"></i>
+    </button>
+
     <NavMenu />
     <Hero />
     <main id="main">
       <About />
       <Facts />
+      <Skills />
+      <Resume />
+      <Portfolio />
+      <Contact />
     </main>
     <Footer />
   </>
