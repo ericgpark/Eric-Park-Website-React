1c48b63c4c79491df6b7ac60aa2d567e60d2a5aa
Eric Park <ericparkmusic@gmail.com>
2021-10-01 00:14:35 -0700

    Implement progress bar animation on waypoint

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import { Waypoint } from 'react-waypoint';
+
 const Skill = ({ skillName, skillVal }) => {
+  const [width, setWidth] = useState(0);
+
+  const handleWaypoint = () => {
+    setWidth(skillVal);
+  }
+
   return (
     <div className="progress">
       <span className="skill">
         {skillName} <i className="val">{skillVal + '%'}</i>
       </span>
       <div className="progress-bar-wrap">
+        <Waypoint onEnter={handleWaypoint} />
         <div
           className="progress-bar"
           role="progressbar"
           aria-valuenow={skillVal}
           aria-valuemin="0"
-          aria-valuemax="100"></div>
+          aria-valuemax="100"
+          style={{width: width + '%'}}
+        ></div>
       </div>
     </div>
   );
