b9145f5b4430e6b03a6eb04a21dc4f1ec162f15b
ericgpark <ericparkmusic@gmail.com>
2025-06-23 12:03:33 -0700

    Update file extension from js to jsx

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// import NavMenu from './containers/NavMenu';
+// import Hero from './containers/Hero';
+// import Footer from './containers/Footer';
+// import About from './containers/About';
+// import Facts from './containers/Facts';
+// import Skills from './containers/Skills';
+// import Resume from './containers/Resume';
+// import Portfolio from './containers/Portfolio';
+// import Contact from './containers/Contact';
+
+// const App = () => (
+//   <>
+//     <button type="button" className="mobile-nav-toggle d-xl-none">
+//       <i className="icofont-navigation-menu"></i>
+//     </button>
+
+//     <NavMenu />
+//     <Hero />
+//     <main id="main">
+//       <About />
+//       <Facts />
+//       <Skills />
+//       <Resume />
+//       <Portfolio />
+//       <Contact />
+//     </main>
+//     <Footer />
+//     <div id="preloader"></div>
+//   </>
+// );
+
+// export default App;
+
+// filepath: f:\Code\Eric-Park-Website-React\src\App.jsx
+export default function App() {
+  return <h1>Hello from App!</h1>;
+}
